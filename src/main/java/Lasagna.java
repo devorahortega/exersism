bc6d8cc05df394e1bede971e1ecd6d1cd2b2b5bc
Devorah Ortega <devorahb.ortega@gmail.com>
2022-12-06 12:24:25 -0600

    Adds first attempt at lasagna problem- the setup

--- src/main/java/Lasagna.java
+++ src/main/java/Lasagna.java
@@ -1,9 +1,21 @@
 public class Lasagna {
     // TODO: define the 'expectedMinutesInOven()' method
-
+    Lasagna lasagna = new Lasagna();
+    lasagna.expectedMinutesInOven();
+    // => 40
+    
     // TODO: define the 'remainingMinutesInOven()' method
+    Lasagna lasagna = new Lasagna();
+    lasagna.remainingMinutesInOven(30);
+// => 10
 
     // TODO: define the 'preparationTimeInMinutes()' method
+    Lasagna lasagna = new Lasagna();
+    lasagna.preparationTimeInMinutes(2);
+    // => 4
 
     // TODO: define the 'totalTimeInMinutes()' method
+    Lasagna lasagna = new Lasagna();
+    lasagna.totalTimeInMinutes(3, 20);
+// => 26
 }
